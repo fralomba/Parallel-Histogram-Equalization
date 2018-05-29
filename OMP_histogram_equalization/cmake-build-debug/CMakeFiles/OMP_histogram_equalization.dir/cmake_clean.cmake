e7f00c7375d6bbef73e1cc7bff9ffe399199b725
fralomba <franci.lomba23894@gmail.com>
2018-05-29 12:28:19 +0200

    first commit

---
+++ OMP_histogram_equalization/cmake-build-debug/CMakeFiles/OMP_histogram_equalization.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OMP_histogram_equalization.dir/main.cpp.o"
+  "OMP_histogram_equalization.pdb"
+  "OMP_histogram_equalization"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OMP_histogram_equalization.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
